b453a7f521eeb66da5f066b54a09eac1709e8d50
sherazanwar21 <sheraz786868@gmail.com>
2024-06-16 01:20:14 +0000

    tf-ansible

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+data "aws_security_group" "allow_httpd-ssh" {
+    name = "allow_httpd-ssh"
+}
+resource "aws_instance" "pipeline-clients" {
+    ami = "ami-033fabdd332044f06"
+    instance_type = "t2.micro"
+    security_groups = [ data.aws_security_group.allow_httpd-ssh ]
+    count = 2
+}
+
+output "instances_ip" {
+    value = aws_instance.pipeline-clients[*].public.ip
+  
+}
